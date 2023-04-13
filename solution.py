e04d1521bc2064bb39fefc864b3a964b04991476
TatianaNahanova <127849913+TatianaNahanova@users.noreply.github.com>
2023-04-13 09:10:58 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 230790372 # Ваш chat ID, не меняйте название переменной
+
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.04
+    _, pvalue = stats.ks_2samp(x, y)
+
+    if pvalue <= alpha:
+        is_rejected = True
+    else:
+        is_rejected = False
+
+    return is_rejected
